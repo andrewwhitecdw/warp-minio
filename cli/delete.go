9303993e9894c025226d2fdaf9f1dafb85105a6d
andrewwhitecdw <andrewwhitecdw@users.noreply.github.com>
2026-08-17 09:34:08 -0500

    fix: The too-few-objects error message contains a double space
    Signed-off-by: andrewwhitecdw <andrewwhitecdw@users.noreply.github.com>

--- cli/delete.go
+++ cli/delete.go
@@ -100,7 +100,7 @@ func checkDeleteSyntax(ctx *cli.Context) {
 	if !ctx.Bool("list-existing") {
 		wantO := ctx.Int("batch") * ctx.Int("concurrent") * 4
 		if ctx.Int("objects") < wantO {
-			console.Fatalf("Too few objects: With current --batch  and --concurrent settings, at least %d objects should be used for a valid benchmark. Use --objects=%d", wantO, wantO)
+			console.Fatalf("Too few objects: With current --batch and --concurrent settings, at least %d objects should be used for a valid benchmark. Use --objects=%d", wantO, wantO)
 		}
 	}
 }
